9dccdf0db9c1509231f3d88640d50bd2a708a263
chaleconetwork <chaleconetworking@gmail.com>
2023-05-29 17:09:54 -0400

    mi asistencia

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const btn = document.getElementById('btnCalcular');
+const salida = document.getElementById('salida');
+
+var resultado = 0;
+var faltar = 0;
+var restantes = 0;
+var aprobar = 0;
+var final = 0;
+var sumaTotal = 0;
+
+btn.addEventListener('click', () => {
+    const presente = parseInt(document.getElementById('presente').value); //7
+    const ausente = parseInt(document.getElementById('ausente').value); //3
+    const total = document.getElementById('total').value; //18
+
+    resultado = (presente / total) * 100; //39%
+    totalAprobar(resultado, total, presente, ausente)
+});
+
+const totalAprobar = (resultado, total, presente, ausente) => {
+    aprobar = (60 / 100) * total // calculo para saber el minimo de asistencias que necesitas para aprobar
+    sumaTotal = presente + ausente
+    if (sumaTotal <= total) {
+        restantes = total - sumaTotal
+        faltar = aprobar - presente
+        final = restantes - faltar
+    
+        console.log(Math.round(restantes))
+        salida.innerHTML =
+            `
+            Tienes ${Math.round(resultado.toString())} % de asistencia <br/>
+            Te quedan ${restantes} días de clases <br/>
+            Puedes faltar ${Math.round(final)} veces más
+            `
+    } else {
+        salida.innerHTML =
+            `
+            Los valores ingresados no son válidos
+            `
+    }
+}
