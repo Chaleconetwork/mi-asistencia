4ec3cda20cd151aca58394cc97067cc5bbf3c00a
chaleconetwork <chaleconetworking@gmail.com>
2023-06-03 12:52:34 -0400

    md19

--- js/app.js
+++ js/app.js
@@ -28,12 +28,12 @@ const totalAprobar = (resultado, total, presente, ausente) => {
     }
 
     if (final < 0)
-        return salida.innerHTML ='No cumples con el mínimo de asistencia para alcanzar el 60%'
+        return salida.innerHTML = 'No cumples con el mínimo de asistencia para alcanzar el 60%';
 
     if (final >= 0)
         return salida.innerHTML = `Tienes ${resultado.toString()} % de asistencia <br/> Te quedan ${restantes} semanas de clases <br/> Puedes faltar 
-        ${final === 1 ? final + ' vez más' : final +' veces más'}`
-    
+        ${final === 1 ? final + ' vez más' : final + ' veces más'}`;
+
     if (sumaTotal > total)
-        return salida.innerHTML ='Los valores ingresados sobrepasan la cantidad de clases'
+        return salida.innerHTML = 'Los valores ingresados sobrepasan la cantidad de clases';
 }
